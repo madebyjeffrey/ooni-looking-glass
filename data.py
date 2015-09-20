07a78009cab29bfaa44121144ef06fb2836b4b39
Tyler Fisher <tyler@tylerfisher.org>
2015-09-20 01:10:32 -0400

    Additional DAL methods. Getting sketchy.

--- data.py
+++ data.py
@@ -6,6 +6,17 @@ from pprint import pprint
 collection = 'ooni_public'
 
 
+def autodict():
+    l = lambda: defaultdict(l)
+    return l()
+
+
+def defaultdict_to_dict(d):
+    if isinstance(d, defaultdict):
+        d = {k: defaultdict_to_dict(v) for k, v in d.iteritems()}
+    return d
+
+
 def get_distinct_asns(db):
     """
     Returns a list of anonymous systems from which bridge_reachability tests have been performed
@@ -13,6 +24,35 @@ def get_distinct_asns(db):
     :return:
     """
     return db[collection].distinct('probe_asn')
+
+
+def get_country_codes(db):
+    return db.ooni_public.distinct('probe_cc')
+
+
+def get_pluggable_transport_metrics_per_country(db):
+    pipeline = [
+        {'$unwind': '$results'},
+        {'$project': {
+            '_id': 0,
+            'results.probe_cc': 1,
+            'results.success': 1,
+            'results.transport_name': 1,
+        }}
+    ]
+    rs = {}
+    for cc in get_country_codes(db=db):
+        rs[cc] = defaultdict(lambda: defaultdict(int))
+    for d in db.ooni_public.aggregate(pipeline):
+        d = d['results']
+        if len(d) != 3:
+            continue
+
+        if d['success']:
+            rs[d['probe_cc']][d['transport_name']]['successfulBridgeConnections'] += 1
+        else:
+            rs[d['probe_cc']][d['transport_name']]['failedBridgeConnections'] += 1
+    return defaultdict_to_dict(rs)
 
 
 def get_number_of_metrics_per_country(db):
@@ -24,6 +64,7 @@ def get_number_of_metrics_per_country(db):
             'results.success': 1
         }}
     ]
+
     rs = defaultdict(lambda: defaultdict(int))
     for d in db.ooni_public.aggregate(pipeline):
         if d['results']['success']:
@@ -43,3 +84,9 @@ def get_mongodb_connection(host, port):
     print("Initiating MongoDB connection to %s:%d" % (host, port))
     client = MongoClient(host, port)
     return client.ooni
+
+
+if __name__ == "__main__":
+    db = get_mongodb_connection(host='buildstuffwith.me', port=27017)
+    metrics = get_pluggable_transport_metrics_per_country(db=db)
+    pprint(metrics)
